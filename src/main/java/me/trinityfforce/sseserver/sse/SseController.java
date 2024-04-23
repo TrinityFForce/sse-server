4cccd331c594c37973ee919d5da368ef427b3558
zapzookj <zapzook@naver.com>
2024-04-23 15:35:40 +0900

    fix : 불필요한 주석 제거

--- src/main/java/me/trinityfforce/sseserver/sse/SseController.java
+++ src/main/java/me/trinityfforce/sseserver/sse/SseController.java
@@ -15,37 +15,12 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RestController
 @RequestMapping("/api/v1/item")
 public class SseController {
-
-    //private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
     private Map<Long, List<SseEmitter>> emitters = new ConcurrentHashMap<>();
-//    @GetMapping(path = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-//    public Flux<ServerSentEvent<String>> streamEvents() {
-//        return Flux.interval(Duration.ofSeconds(1))
-//            .map(sequence -> ServerSentEvent.<String>builder()
-//                .id(String.valueOf(sequence))
-//                .event("message")
-//                .data("Hello from server: " + sequence)
-//                .build());
-//    }
 
     @GetMapping("/subscribe/{itemId}")
     public SseEmitter subscribe(@PathVariable Long itemId) {
         // 새로운 SseEmitter 생성
         SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
-
-//        emitter.onCompletion(()->{
-//            synchronized (this.emitters){
-//                this.emitters.remove(emitter);
-//            }
-//        });
-
-        // 맵에 저장
-        //emitters.put(itemId, emitter);
-
-        // 연결 종료 시 맵에서 제거
-//        emitter.onCompletion(() -> emitters.remove(itemId));
-//        emitter.onTimeout(() -> emitters.remove(itemId));
-//        emitter.onError(e -> emitters.remove(itemId));
 
         emitter.onCompletion(() -> removeSubscriber(itemId, emitter));
         emitter.onTimeout(() -> removeSubscriber(itemId, emitter));
@@ -60,17 +35,6 @@ public class SseController {
     public void sendItemUpdate(Long itemId, Object data) {
         List<SseEmitter> subscribers = emitters.getOrDefault(itemId, Collections.emptyList());
 
-//        if (subscribers != null) {
-//            subscribers.forEach(s -> {
-//                try {
-//                    if (s != null) {
-//                        s.send(SseEmitter.event().name("itemUpdate").data(data).build());
-//                    }
-//                } catch (IOException e) {
-//                    s.completeWithError(e);
-//                }
-//            });
-//        }
         for (SseEmitter s : new ArrayList<>(subscribers)) {
             try {
                 s.send(SseEmitter.event().name("itemUpdate").data(data).build());
